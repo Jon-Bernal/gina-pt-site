e8803150d83c97e15728dacc6e2c572df1ea577a
jon Bernal <jbernalwrc@gmail.com>
2024-04-14 22:33:17 -0700

    add: content to landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to Prism PT!</h1>
-<p>We're still under construction, please bear with us!</p>
+<h2>Pardon our dust! </h2>
+<h1>
+  Coming soon, Prism Physical Therapy website and clinic.
+</h1>
+<p>
+  We're located in Clinton Washington. If you would like to join our non-spammy mailing list for updates, please enter your information below. We look forward to working with you soon.
+</p>
+
