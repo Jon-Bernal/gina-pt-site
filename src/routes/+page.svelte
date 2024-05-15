fcd6d76a1b4a90d90fddb4589cd1752c4c663916
jon Bernal <jbernalwrc@gmail.com>
2024-05-14 22:05:51 -0700

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,6 @@
-<h2>Pardon our dust! </h2>
-<h1>
-  Coming soon, Prism Physical Therapy website and clinic.
-</h1>
+<h2>Pardon our dust!</h2>
+<h1>Coming soon, Prism Physical Therapy website and clinic.</h1>
 <p>
-  We're located in Clinton Washington. If you would like to join our non-spammy mailing list for updates, please enter your information below. We look forward to working with you soon.
+	We're located in Clinton Washington. If you would like to join our non-spammy mailing list for
+	updates, please enter your information below. We look forward to working with you soon.
 </p>
-
--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -1,65 +1,147 @@
-<h1>FAQ</h1>
-<section>
-	<h3>What Does a Physical Therapist Do?</h3>
-	<p>
-		I provide therapy in orthopedics rehab, whether you are recovering from an injury or are wanting
-		to gain the ability to do more physical activity. This includes exercise that focuses both on
-		individual muscles as well as muscle chains. I work to build the body up from the basics to
-		ensure there is no oversight of underlying causes of pain and injury. This then allows the body
-		the best opportunity to achieve its full potential.
-	</p>
-	<p>
-		I work with the healing process of inflammation. This includes inflammation management through
-		ice/heat, TENS unit usage, and manual therapy. I maximize on the opportunities available during
-		the healing cycles with the various techniques listed here.
-	</p>
-	<p>
-		I also provide Fascial Counterstrain, a hands-on treatment that aides the body in recovery by
-		releasing inflammation and releasing tension of connective tissues throughout.
-	</p>
-</section>
+<script lang="ts">
+	import * as Accordion from '$lib/components/ui/accordion/index.js';
+</script>
 
-<section>
-	<h3>What is Fascial Counterstrain?</h3>
-	<p>
-		Fascial Counterstrain (FCS) is the application of hands-on manual therapy to release
-		contractions of your tissues, such as muscles, ligaments, and nerves. When there is tissue
-		tension, this can cause pain and dysfunction. By releasing these tissues, the body can move more
-		appropriately and release inflammation. This is a painless technique and has the ability to
-		affect any system in the body, including lymphatics, visceral organs, bone, and more. Learn more
-		about FCS here.
-	</p>
-</section>
+<main>
+	<h2>FAQ</h2>
+	<div class="faq">
+		<div>
+			<h3>What Does a Physical Therapist Do?</h3>
+			<p>
+				I provide therapy in orthopedics rehab, whether you are recovering from an injury or are
+				wanting to gain the ability to do more physical activity. This includes exercise that
+				focuses both on individual muscles as well as muscle chains and groups. I work to build the
+				body up from the basics to ensure there is no oversight of underlying causes of pain and
+				injury. This then allows the body the best opportunity to achieve its full potential.
+			</p>
+			<p>
+				I work with the healing process of inflammation. This includes inflammation management
+				through ice/heat, TENS unit usage, and manual therapy. I maximize the opportunities
+				available during the healing cycles with the various techniques listed here.
+			</p>
+			<p>
+				I also provide Fascial Counterstrain, a hands-on treatment that aids the body in recovery by
+				releasing inflammation and releasing tension of connective tissues throughout.
+			</p>
+		</div>
 
-<section>
-	<h3>Do you take insurance?</h3>
-	<p>
-		At this time, I do not accept insurance. I am however happy to provide a superbill so that you
-		may request reimbursement from your insurance provider.
-	</p>
-</section>
+		<div>
+			<h3>What is Fascial Counterstrain?</h3>
+			<p>
+				Fascial Counterstrain (FCS) is the application of hands-on manual therapy to release
+				contractions of your tissues, such as muscles, ligaments, and nerves. When there is tissue
+				tension, this can cause pain and dysfunction. By releasing these tissues, the body can move
+				more appropriately and release inflammation. This is a painless technique and has the
+				ability to affect any system in the body, including lymphatics, visceral organs, bone, and
+				more. Learn more about FCS <a href="https://counterstrain.com/">here</a>.
+			</p>
+		</div>
 
-<section>
-	<h3>What is your approach to patient care?</h3>
-	<p>
-		My approach is to appreiciate all aspects of healthcare. While I am only licensed to provide
-		physical therapy, I will recommend other types of therapy and practitioners of these therapies
-		as the need arises. I believe in being honest and open about what I am seeing, and from there
-		providing options for my patients to explore to further help them along their health journey. I
-		recognize that many people have had unpleasant experiences in the healthcare system, and have
-		suffered trauma from this. I promise to ensure my patients feel safe, heard, and understood
-		(regardless of ethnicity, gender identity, sexuality, ability, or other intersectional
-		identities) when working with me.
-	</p>
-</section>
+		<div>
+			<h3>Do you take insurance or Medicare?</h3>
+			<p>
+				At this time, I do not accept insurance or Medicare. I am however happy to provide a
+				superbill so that you may request reimbursement from your insurance provider. Medicare rules
+				state that those who are medicare beneficiaries cannot pay “out of pocket” for physical
+				therapy. However, there are circumstances that allow a person with Medicare to pay
+				privately. Please email for more details.
+			</p>
+		</div>
 
-<section>
-	<h3>Are we a good fit to work together?</h3>
-	<p>
-		I look at physical therapy as a team effort. I will do everything in my power to help someone
-		heal, but I cannot do it alone. If the patient is unable to fully participate (within reason), I
-		will not be able to continue working with them as we will struggle to see results. If you want
-		to dive deep into your health and are willing to be responsible for your health, we can achieve
-		great results.
-	</p>
-</section>
+		<div>
+			<h3>What are my options for payment?</h3>
+			<p>
+				Payment is due at the time of service for all appointments, and I can provide a superbill
+				upon request. For those with a Health Savings Account (HSA) or a Flexible Spending Account
+				(FSA) you may also use this as a form of payment if physical therapy is determined to be
+				necessary by your doctor.
+			</p>
+		</div>
+
+		<div>
+			<h3>Why choose to pay privately for physical therapy?</h3>
+			<p>
+				I have chosen to provide physical therapy out of network in order to stay aligned with my
+				values and ethics for patient care. This allows me to address a person’s needs as they arise
+				rather than being isolated to one part of the body at a time. Paying privately also allows
+				me to treat for longer periods of time per session and allows me to treat a person for as
+				many sessions as they need to heal. Treating someone for longer sessions provides more time
+				to address all aspects of care, and reduces the number of appointments needed to achieve the
+				patient’s goals.
+			</p>
+		</div>
+
+		<div>
+			<h3>What are your rates?</h3>
+			<p>All of my sessions, both evaluations and treatments, are 90 minutes for $225.</p>
+		</div>
+
+		<div>
+			<h3>Do I need a referral for physical therapy?</h3>
+			<p>
+				You do not need a referral to see a physical therapist in the state of Washington as we are
+				a “direct access” state. If you plan to submit for reimbursement to your insurance company,
+				you may be required to have a referral to receive reimbursement. You may also have a limited
+				number of visits for physical therapy per year. Be sure to check with your insurance
+				provider on what your benefits cover.
+			</p>
+		</div>
+
+		<div>
+			<h3>What is your approach to patient care?</h3>
+			<p>
+				My approach is to appreiciate all aspects of healthcare. While I am only licensed to provide
+				physical therapy, I will recommend other types of therapy and practitioners of these
+				therapies as the need arises. I believe in being honest and open about what I am seeing, and
+				from there providing options for my patients to explore to further help them along their
+				health journey. I recognize that many people have had unpleasant experiences in the
+				healthcare system, and have suffered trauma from this. I promise to ensure my patients feel
+				safe, heard, and understood (regardless of ethnicity, gender identity, sexuality, ability,
+				or other interdival identities) when working with me.
+			</p>
+		</div>
+
+		<div>
+			<h3>Are we a good fit to work together?</h3>
+			<p>
+				I look at physical therapy as a team effort. I will do everything in my power to help
+				someone heal, but I cannot do it alone. If the patient is unable to fully participate
+				(within reason), I will not be able to continue working with them as we will struggle to see
+				results. If you want to dive deep into your health and are willing to be responsible for
+				your health, we can achieve great results.
+			</p>
+		</div>
+	</div>
+</main>
+
+<Accordion.Root class="w-full sm:max-w-[70%]">
+	<Accordion.Item value="item-1">
+		<Accordion.Trigger>Is it accessible?</Accordion.Trigger>
+		<Accordion.Content>Yes. It adheres to the WAI-ARIA design pattern.</Accordion.Content>
+	</Accordion.Item>
+	<Accordion.Item value="item-2">
+		<Accordion.Trigger>Is it styled?</Accordion.Trigger>
+		<Accordion.Content>
+			Yes. It comes with default styles that matches the other components' aesthetic.
+		</Accordion.Content>
+	</Accordion.Item>
+	<Accordion.Item value="item-3">
+		<Accordion.Trigger>Is it animated?</Accordion.Trigger>
+		<Accordion.Content>
+			Yes. It's animated by default, but you can disable it if you prefer.
+		</Accordion.Content>
+	</Accordion.Item>
+</Accordion.Root>
+
+<style>
+	h2 {
+		font-size: 2rem;
+	}
+	main {
+		display: flex;
+		flex-direction: column;
+		padding: 40px;
+		justify-content: center;
+		width: 100%;
+	}
+</style>
