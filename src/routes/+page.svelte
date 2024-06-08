0d75dfa66573c593da5f0d8c4c3488caa792bfd7
Jon Bernal <jbernalwrc@gmail.com>
2024-06-08 11:34:39 -0700

    adjust style of radio group to better match the rest of the form.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,7 +15,7 @@
 <h1>Coming soon, Prism Physical Therapy website and clinic.</h1>
 
 <p>
-	We're located in Clinton Washington. If you would like to join our non-spammy mailing list for
+	We're located in Clinton, Washington. If you would like to join our non-spammy mailing list for
 	updates, please enter your information below. We look forward to working with you soon.
 </p>
 
@@ -42,7 +42,8 @@
 		label={'Are you a returning patient?'}
 		id="returning"
 		name="returning"
-		style={'align-self: start'}
+		style={'align-self: start;'}
+		size="md"
 		items={[
 			{ label: 'Yes', value: 'yes' },
 			{ label: 'No', value: 'no' }
